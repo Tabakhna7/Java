e5e8ce638cf2eaac6c4bb90055e3133e5b87eeca
Tabakhna7 <Tabakhna7@gmail.com>
2024-06-27 15:56:36 +0300

    added daikichi path variables

--- Daikichi/src/main/java/com/axsos/Daikichi/DaikichiController.java
+++ Daikichi/src/main/java/com/axsos/Daikichi/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.axsos.Daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,31 @@ public class DaikichiController {
         public String tomorrow() {
                 return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!'.";
         }
+	 	@RequestMapping("/daikichi/travel/{capital}")
+		public String render2 (@PathVariable("capital")String capital) {
+			
+			
+	    return " Congrats ! you will soon travel to "+ capital;
+		}
+	 	@RequestMapping("/daikichi/lotto/{odd}")
+	 	public String render3(@PathVariable("odd")int odd) {
+	 		
+	 		if ( odd %2!=0) {
+	 			return" You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends" ;
+	 		}
+	 		
+	 		else if ( odd %2==0) {
+	 			return "you will take a grand journy in the near future , but be wary of tempting offers";
+	 			
+	 		}
+	 		
+	 		else {
+	 			return " None";
+	 		}
+	 		
+	 		
+	 		
+	 	}
 }
 
 
