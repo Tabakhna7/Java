20f629a8ebd7b5981475759fe71be0b43c71a0a6
Tabakhna7 <Tabakhna7@gmail.com>
2024-07-01 16:32:27 +0300

    added allbooks

--- allbooks-1/src/main/java/com/codingdojo/allbooks_1/controllers/BookController.java
+++ allbooks-1/src/main/java/com/codingdojo/allbooks_1/controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.codingdojo.allbooks_1.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.codingdojo.allbooks_1.models.Book;
 import com.codingdojo.allbooks_1.services.BookService;
@@ -24,4 +27,11 @@ public class BookController {
             return "redirect:/books";
         }
     }
+    @RequestMapping("/books")
+    public String index(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "books.jsp";
+    }
 }
+
